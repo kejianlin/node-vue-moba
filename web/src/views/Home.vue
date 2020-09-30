d9ae747932f069239b9d370db53beba3147dde24
kejianlin <13163258663@163.com>
2020-09-30 18:04:30 +0800

    移动端官网

---
+++ web/src/views/Home.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg" alt>
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg" alt>
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg" alt>
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-2" slot="pagination"></div>
+    </swiper>
+    <!-- end of swiper -->
+    <div class="nav-icons bg-white mt-3 text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 10" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
+    <!-- end of nav icons -->
+    <div class="card bg-white p-3 mt-3">
+      <div class="card-header d-flex ai-center">
+        <i class="iconfont icon-menu1"></i>
+        <div class="fs-xl flex-1 px-2">新闻资讯</div>
+        <i class="iconfont icon-menu"></i>
+      </div>
+      <div class="card-body pt-3">
+        <div class="nav jc-between">
+          <div class="nav-item active">
+            <div class="nav-link">热门</div>
+          </div>
+          <div class="nav-item">
+            <div class="nav-link">新闻</div>
+          </div>
+          <div class="nav-item">
+            <div class="nav-link">新闻</div>
+          </div>
+          <div class="nav-item">
+            <div class="nav-link">新闻</div>
+          </div>
+          <div class="nav-item">
+            <div class="nav-link">新闻</div>
+          </div>
+        </div>
+        <div class="pt-3">
+          <swiper>
+            <swiper-slide  v-for="m in 5" :key="m">
+              <div class="py-2" v-for="n in 5" :key="n">
+                <span>[新闻]</span>
+                <span>|</span>
+                <span>深圳市乐讯物流供应链有限公司</span>
+                <span>06/02</span>
+              </div>
+            </swiper-slide>
+          </swiper>
+        </div>
+        
+      </div>
+    </div>
+    
+    <!-- <i class="iconfont icon-news text-primary"></i> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".pagination-home"
+        }
+      },
+
+    };
+  },
+}
+</script>
+
+<style lang="scss">
+@import '../assets/scss/variables';
+
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: map-get($colors, 'white');
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, 'info');
+    }
+  }
+}
+
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
+    }
+  }
+}
+
+</style>
